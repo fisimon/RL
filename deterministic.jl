7a8049ce1d72499297e0ef88bdd57a2f0ead7e8e
Felipe <fisimon@uc.cl>
2017-03-05 14:14:09 -0300

    add recursive value iteration

--- deterministic.jl
+++ deterministic.jl
@@ -5,13 +5,14 @@ E = [ 0 0 0 0; 0 1 0 0; 0 0 0 0]
 # this are the reward for being in any given state
 R = [ 0 0 0 1; 0 0 0 -1; 0 0 0 0]
 
+n, m = size(R)
 # This are the actions
 A = ["UP", "DOWN", "RIGHT", "LEFT"]
 
 # This are all the possible states
-S = reshape([(a,b) for a=1:3, b=1:4], 12)
+S = reshape([(a,b) for a=1:n, b=1:m], 12)
 
-
+# this is the transition function
 function move(s, a, R)
   out = s
   if a == "DOWN"
@@ -33,7 +34,7 @@ end
 
 
 
-
+# aux function to see if it is possible to get to a state s2 from s1
 function ispossible(s1, s2, a, R)
   res = 0
   if s2 == move(s1, a, R)[1]
@@ -43,6 +44,7 @@ function ispossible(s1, s2, a, R)
 end
 
 
+# helper function. Return the best action from s given Utility U
 function get_max_action(s, A, U, R)
   m = -999
   for a in A
@@ -54,6 +56,8 @@ function get_max_action(s, A, U, R)
   return m
 end
 
+
+# transforms a given U[i,j] to a policy for state (i,j)
 function get_policy(s,U, A, R)
   m = -9999
   action = ""
@@ -68,6 +72,7 @@ function get_policy(s,U, A, R)
 end
 
 
+# value iteration function
 function value_iteration(U, R, S)
   for s in S
     U[s[1], s[2]] = R[s[1], s[2]] + γ * get_max_action(s,A, U, R)
@@ -75,10 +80,26 @@ function value_iteration(U, R, S)
   return U
 end
 
+# Recursive implementation of value iteration
+function value_iteration(U, R, S, current, finish)
+  if current == finish
+    return U
+  else
+    for s in S
+      # Bellmans equation
+      U[s[1], s[2]] = R[s[1], s[2]] + γ * get_max_action(s,A, U, R)
+      # we can simple use get_max_action since its a Deterministic world
+    end
+    return value_iteration(U,R,S,current + 1, finish)
+  end
+end
 
+
+# constants
 γ = 0.1
+n_iteration = 100
+# starting Utilities are all 0
 U = zeros(size(R))
 
-
-U = value_iteration(U, R, S)
-reshape(map(x->get_policy(x,U, A,R), S), (3,4))
+U = value_iteration(U, R, S, 0, n_iteration)
+policies = reshape(map(x->get_policy(x,U, A,R), S), (3,4))
